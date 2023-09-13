5337ba905f86d66c65976f18b36dd529c9d6825a
shadman2588 <143462571+shadman2588@users.noreply.github.com>
2023-09-14 09:12:49 +1200

    Add files via upload

---
+++ 02_variable.py
@@ -0,0 +1,12 @@
+# code to learn about variable
+
+# variable can be string
+greeting = "hello world"
+
+# they can also be number
+fav_num = 7
+print(fav_num * 2)
+
+num_string = "5"
+print(num_string * 5)
+
